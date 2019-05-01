b989cffdcf56fb8cbb2b9a101557056d3081ef87
Mushfiqur Rahman <42231885+mushfiq344@users.noreply.github.com>
2019-05-01 11:52:05 +0600

    Create again_twentyfive.cpp

---
+++ again_twentyfive.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n,sum=1,i;
+    cin>>n;
+    if (n==0){cout<<"01";return 0;}
+    else if(n==1){  cout<<"05";return 0; }
+    else cout<<"25";
+}
